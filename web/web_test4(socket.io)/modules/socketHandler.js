d6818394ba46fb9d78bf5f8c300ea43fb6dfd82c
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2024-11-06 19:21:48 +0900

    1106 1921

--- web/web_test4(socket.io)/modules/socketHandler.js
+++ web/web_test4(socket.io)/modules/socketHandler.js
@@ -2,22 +2,49 @@ const { Server } = require('socket.io');
 
 const socketHandler = (server) => {
     const io = new Server(server); // 접속 시 서버에서 실행되는 코드
+    let user = {}; //현재 접속중인 유저
+    //주석
     io.on('connection', (socket) => {
         const req = socket.request; //리퀘스트의 내용들
-        const socket_id = socket.id; //소켓의 고유한 아이디 발급
-        const client_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; //클라의 IP 확인가능
+        const socket_id = socket.id; //클라의 소켓 id
+        const client_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; //클라의 IP 
         console.log("connection !!");
         console.log("socket ID : ", socket_id);
         console.log("client IP : ", client_ip);
 
-        //이벤트에 대한 콜백 함수들
-        socket.on("disconnect", () => { // 사전에 제공되는 callback (disconnect, error):클라 연결 해제 시 콜백
+        user[socket.id] = { nickname: "user nickname", point: 0 } //서버에 접속할떄 유저 객체에 넣음(현재 접속중인 인원 확인)
+
+        //연결 해제시 콜백(사전에 정의된 이벤트)
+        socket.on("disconnect", () => {
             console.log(socket_id, "client disconnected");
+            delete user[socket.id];
         });
 
-        socket.on("event1", (msg) => { // 생성한 이벤트 이름 event1 호출 시 콜백
-            console.log(msg);    //단순히 받은 메세지 출력
-            io.emit("msg","전체 메세지") //모든 클라에 메세지 전송
+        // event1이벤트 발생 시 콜백
+        socket.on("event1", (msg) => {
+            console.log(msg);
+            socket.emit("getID", socket.id) //메세지를 보낸 클라에게 보냄
+        });
+
+
+
+        //모두에게
+        socket.on("input", (data) => {
+            io.emit("msg", { id: socket.id, message: data }); //모든 클라에게 메세지 보냄
+            console.log(socket.id, "가 보낸 메세지", data);
+            console.log(user);
+        });
+
+        //본인 제외한 모든 소켓
+        socket.on("inputWM", (data) => {
+            socket.broadcast.emit("msg", { id: socket.id, message: data });
+            console.log(data, "를 받았는데, 본인 빼고 boadcast 해야함")
+        })
+
+        //특정 소켓
+        socket.on("privatemsg", (data, id) => {
+            io.to(id).emit("msg", { id: socket.id, message: data });
+            console.log(socket.id, "가", id, "에게 보내는 메세지 : ", data);
         });
     });
 };
